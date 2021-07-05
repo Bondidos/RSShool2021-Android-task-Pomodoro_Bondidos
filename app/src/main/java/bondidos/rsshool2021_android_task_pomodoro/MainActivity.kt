67c99446e0795cb0c2c945d473801d39ba772f3a
Bondidos <gglbond@gmail.com>
2021-07-05 23:57:42 +0300

    7.5.2021 begining

--- app/src/main/java/bondidos/rsshool2021_android_task_pomodoro/MainActivity.kt
+++ app/src/main/java/bondidos/rsshool2021_android_task_pomodoro/MainActivity.kt
@@ -1,9 +1,13 @@
-package Bondidos.rsshool2021_android_task_pomodoro
+package bondidos.rsshool2021_android_task_pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import bondidos.rsshool2021_android_task_pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
