f19b0a28b9bec3dd4a2fa66aa5c533dfde91da8d
Bondidos <gglbond@gmail.com>
2021-07-05 21:56:09 +0300

    Initial commit

---
+++ app/src/main/java/Bondidos/rsshool2021_android_task_pomodoro/MainActivity.kt
@@ -0,0 +1,11 @@
+package Bondidos.rsshool2021_android_task_pomodoro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
